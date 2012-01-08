8e266f85d9c33686c2797520d48489ea13802cf1
Jeff Standen <jeff@webgroupmedia.com>
2012-01-07 16:03:57 -0800

    Added Virtual Attendant behavior + macro variable support

--- api/events/feeditem_macro.php
+++ api/events/feeditem_macro.php
@@ -6,13 +6,14 @@ class Event_FeedItemMacro extends AbstractEvent_FeedItem {
 		$this->_event_id = self::ID;
 	}
 	
-	static function trigger($trigger_id, $item_id) {
+	static function trigger($trigger_id, $item_id, $variables=array()) {
 		$events = DevblocksPlatform::getEventService();
 		$events->trigger(
 	        new Model_DevblocksEvent(
 	            self::ID,
                 array(
                     'item_id' => $item_id,
+                    '_variables' => $variables,
                 	'_whisper' => array(
                 		'_trigger_id' => array($trigger_id),
                 	),
