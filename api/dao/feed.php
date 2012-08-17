c861b2f375ee9394b57032ba8159a6626697e11a
Jeff Standen <jeff@webgroupmedia.com>
2012-08-17 14:08:54 -0700

    6.1 compatibility

--- api/dao/feed.php
+++ api/dao/feed.php
@@ -144,15 +144,21 @@ class DAO_Feed extends C4_ORMHelper {
 	
 		// Translate virtual fields
 		
+		$args = array(
+			'join_sql' => $join_sql,
+			'where_sql' => $where_sql,
+			'has_multiple_values' => $has_multiple_values
+		);
+		
 		array_walk_recursive(
 			$params,
 			array('DAO_Feed', '_translateVirtualParameters'),
-			array(
-				'join_sql' => &$join_sql,
-				'where_sql' => &$where_sql,
-				'has_multiple_values' => &$has_multiple_values
-			)
+			&$args
 		);
+		
+		$join_sql = $args['join_sql'];
+		$where_sql = $args['where_sql'];
+		$has_multiple_values = $args['has_multiple_values'];
 		
 		return array(
 			'primary_table' => 'feed',
