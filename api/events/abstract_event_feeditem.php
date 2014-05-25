3f76d1e77f59f48d42686e86311fe914c39b418d
Jeff Standen <jeff@webgroupmedia.com>
2014-05-25 12:08:43 -0700

    6.7.x compatibility

--- api/events/abstract_event_feeditem.php
+++ api/events/abstract_event_feeditem.php
@@ -86,10 +86,12 @@ abstract class AbstractEvent_FeedItem extends Extension_DevblocksEvent {
 			'item_watchers' => array(
 				'label' => 'Feed item watchers',
 				'context' => CerberusContexts::CONTEXT_WORKER,
+				'is_multiple' => true,
 			),
 			'item_feed_watchers' => array(
 				'label' => 'Feed watchers',
 				'context' => CerberusContexts::CONTEXT_WORKER,
+				'is_multiple' => true,
 			),
 			/*
 			'item_feed_id' => array(
@@ -124,14 +126,14 @@ abstract class AbstractEvent_FeedItem extends Extension_DevblocksEvent {
 		return $conditions;
 	}
 	
-	function renderConditionExtension($token, $trigger, $params=array(), $seq=null) {
+	function renderConditionExtension($token, $as_token, $trigger, $params=array(), $seq=null) {
 		$tpl = DevblocksPlatform::getTemplateService();
 		$tpl->assign('params', $params);
 
 		if(!is_null($seq))
 			$tpl->assign('namePrefix','condition'.$seq);
 		
-		switch($token) {
+		switch($as_token) {
 			case 'item_link':
 				$contexts = Extension_DevblocksContext::getAll(false);
 				$tpl->assign('contexts', $contexts);
@@ -148,10 +150,10 @@ abstract class AbstractEvent_FeedItem extends Extension_DevblocksEvent {
 		$tpl->clearAssign('params');
 	}
 	
-	function runConditionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+	function runConditionExtension($token, $as_token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
 		$pass = true;
 		
-		switch($token) {
+		switch($as_token) {
 			case 'item_link':
 				$not = (substr($params['oper'],0,1) == '!');
 				$oper = ltrim($params['oper'],'!');
@@ -159,7 +161,7 @@ abstract class AbstractEvent_FeedItem extends Extension_DevblocksEvent {
 				$from_context = null;
 				$from_context_id = null;
 				
-				switch($token) {
+				switch($as_token) {
 					case 'item_link':
 						$from_context = CerberusContexts::CONTEXT_FEED_ITEM;
 						@$from_context_id = $dict->item_id;
@@ -200,7 +202,7 @@ abstract class AbstractEvent_FeedItem extends Extension_DevblocksEvent {
 				$not = (substr($params['oper'],0,1) == '!');
 				$oper = ltrim($params['oper'],'!');
 				
-				switch($token) {
+				switch($as_token) {
 					case 'item_feed_watcher_count':
 						$value = count($dict->item_feed_watchers);
 						break;
