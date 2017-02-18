0d8d0afb2452aed28168c19f15161832f30c9a2b
Jeff Standen <jeff@webgroupmedia.com>
2017-02-18 00:31:46 -0800

    [Code Cleanup] Fixed an issue with lazy-loading record links from the dictionary.

--- api/dao/feed.php
+++ api/dao/feed.php
@@ -753,7 +753,7 @@ class Context_Feed extends Extension_DevblocksContext implements IDevblocksConte
 		switch($token) {
 			case 'links':
 				$links = $this->_lazyLoadLinks($context, $context_id);
-				$values = array_merge($values, $fields);
+				$values = array_merge($values, $links);
 				break;
 			
 			case 'watchers':
